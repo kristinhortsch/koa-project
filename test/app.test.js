af1ad2a6b6f0ea05aa7794df3a1604c738788de5
Kristin Hortsch <kristinhortsch@gmail.com>
2019-01-28 14:50:44 -0800

    PASSING SECOND TEST

--- test/app.test.js
+++ test/app.test.js
@@ -3,6 +3,15 @@ const request = require('supertest');
 const app = require('../server.js');
 const mongoose = require('mongoose');
 
+const createShoe = async(shoe) => {
+  await request(app.callback())
+    .post('/nike')
+    .send({
+      shoe: shoe,
+      type: 'Running Shoe',
+      price: '$180'
+    });
+};
 describe('nike app', () => {
   beforeEach(done => {
     return mongoose.connection.dropDatabase(() => {
@@ -25,6 +34,13 @@ describe('nike app', () => {
       __v: 0, 
       _id: expect.any(String)
     });
+  });
+
+  it('can list all the nike shoes in the database', async() => {
+    const shoes = ['pegasus', 'air force mids', 'air jordans', 'tanjun'];
+    await Promise.all(shoes.map(createShoe));
+    const response = await request(app.callback()).get('/nike');
+    expect(response.body).toHaveLength(4);
   });
 });
 
